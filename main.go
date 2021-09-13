4a8653a14f87b89fa49f89f2d298ec4d2996cad1
leandro.lomb <leandro.lombi@synchro.com.br>
2021-09-12 21:42:05 -0300

    Now, main routine is calling a function

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+
+	directionReduce "github.com/direction-challenge/directionReduce"
+)
+
+func main() {
+	list := []string{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}
+	result, err := directionReduce.DirReduc(list)
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+
+	fmt.Println(result)
 }
